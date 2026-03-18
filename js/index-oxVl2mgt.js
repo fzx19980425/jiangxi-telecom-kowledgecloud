be4bd6fe58795b2b749575d612fb456910779efd
feng.zexiang <feng.zexiang@ustcinfo.com>
2026-03-18 10:07:57 +0800

    fix: 修改

--- js/index-oxVl2mgt.js
+++
@@ -1,1 +0,0 @@
-import{s}from"./index-BBoh1gHp.js";import{D as o}from"./index-COiDiFxf.js";const r=o(s);export{r as C};
--- js/use-id-D6NX3nBD.js
+++
@@ -1,1 +0,0 @@
-import{av as n}from"./index-COiDiFxf.js";let o=0;function t(){const t=n(),{name:e="unknown"}=(null==t?void 0:t.type)||{};return`${e}-${++o}`}export{t as u};
